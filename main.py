b0b44c4e6d3521261c70561cb2cf072bcf4fd1e7
jasonberylmini <jasonberylmini@gmail.com>
2025-03-13 11:28:42 +0530

    fix invalid input

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ items = menu.get_items()
 
 while is_on:
     choice = input(f"What woulḍ you like? ({items}): ").lower()
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        coffeemaker.report()
-        profit = moneymachine.report()
+    if choice in items:
+        if choice == "off":
+            is_on = False
+        elif choice == "report":
+            coffeemaker.report()
+            profit = moneymachine.report()
+        else:
+            drink = menu.find_drink(choice)
+            if coffeemaker.is_resource_sufficient(drink) and moneymachine.make_payment(drink.cost):
+               coffeemaker.make_coffee(drink)
     else:
-        drink = menu.find_drink(choice)
-        if coffeemaker.is_resource_sufficient(drink) and moneymachine.make_payment(drink.cost):
-           coffeemaker.make_coffee(drink)
-
+        print("Invalid choice.")
